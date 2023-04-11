177d6d5c4c1af90fd05e1fae9b020eafd9b6e8d2
Ulric Padonou <padonouange45@gmail.com>
2023-04-11 14:53:07 +0200

    Maj Projet

--- Partie_back_office_Gestion_des_congés_payés/Program.cs
+++ Partie_back_office_Gestion_des_congés_payés/Program.cs
@@ -16,7 +16,7 @@ namespace Partie_back_office_Gestion_des_congés_payés
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new PageConnexion());
         }
     }
 }
